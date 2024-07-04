6dc96ce89037bb8714149585f2f63a9c769b8468
“umar-phaedra” <umar.farooq@phaedrasolutions.com>
2024-07-04 18:11:38 +0500

    - DB shifted to Atlas

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MongooseModule} from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/easygenerator')],
+  // imports: [MongooseModule.forRoot('mongodb://localhost/easygenerator')], //TODO: Uncomment this for local db
+  imports: [MongooseModule.forRoot('mongodb+srv://umarrhere:Qyc5zzB6meFEbjK5@testcluster.qhvzf2e.mongodb.net/easygenerator')],
 })
 export class DatabaseModule {}
